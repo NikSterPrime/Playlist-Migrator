4479cd7f97b6fee94348a5584f780ac3ff3dfc87
Aditya Puranik <mail.adityapuranik@gmail.com>
2025-08-08 21:40:11 +0530

    Phase 1.2

---
+++ migrator/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+    return (
+        <nav>
+            <h3>Migrator</h3>
+        </nav>
+    )
+};
+
+export default Navbar;
